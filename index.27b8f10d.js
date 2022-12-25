4105dfba2e288711acff40a8c4b26a2326ca80a6
DmytroRudenko11 <117588754+DmytroRudenko11@users.noreply.github.com>
2022-12-25 17:46:13 +0000

    Deploying to gh-pages from @ DmytroRudenko11/IceCream@dac6b593a3d3ee264c9c170374ce17bf71f8c1df 🚀

---
+++ index.27b8f10d.js
@@ -0,0 +1,2 @@
+var scrollBtn=document.querySelector("scrollUp");window.onscroll=function(){window.scrollY>100?scrollBtn.classList.remove("hide"):window.scrollY<100&&scrollBtn.classList.add("hide")};
+//# sourceMappingURL=index.27b8f10d.js.map
